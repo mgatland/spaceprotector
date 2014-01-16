702be86a181c12805dbbebcfec399afa7cc2ee61
Matthew Gatland <mgatland@gmail.com>
2014-01-17 02:54:50 +1300

    when a shot hits a wall the shot disappears

--- js/game.js
+++ js/game.js
@@ -62,14 +62,24 @@
 		var Shot = function (pos, dir) {
 			this.pos = pos;
 			this.dir = dir;
+			this.size = new Pos(5,1);
 			this.pos.moveXY(2,1);
+			this.live = true;
+
 			if (dir === Dir.LEFT) {
 				this.pos.moveXY(-8, 0);
 			} else {
 				this.pos.moveXY(3, 0);
 			}
 			this.update = function () {
+				if (this.live === false) return;
 				this.pos.moveInDir(this.dir, 2);
+				var left = isPointColliding(this.pos, map);
+				var right = isPointColliding(this.pos.clone().moveXY(this.size.x,0), map);
+				if (left || right) {
+					//destroy it
+					this.live = false;
+				}
 			}
 		}
 
@@ -207,7 +217,7 @@
 			painter.drawSprite(man.pos.x,man.pos.y, manSprite0, "#FFFF00");
 
 			shots.forEach(function (shot) {
-				painter.drawSprite(shot.pos.x, shot.pos.y, shotSprite0, "#FFFF00");
+				if (shot.live) painter.drawSprite(shot.pos.x, shot.pos.y, shotSprite0, "#FFFF00");
 			});
 
 			map.forEach(function (row, y) {
