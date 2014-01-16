109c84dd12ab2546ea3df41638612a89c7e47997
Matthew Gatland <mgatland@gmail.com>
2014-01-16 22:42:15 +1300

    man collides with world

--- js/game.js
+++ js/game.js
@@ -3,10 +3,7 @@
 
 	window.initGame = function () {
 
-		var man = {};
-		man.pos = new Pos(10,70);
-		man.size = new Pos(5,5);
-		man.state = "falling";
+		var tileSize = 10;
 
 		var mapData =
 		"O                 O\n" +
@@ -44,9 +41,54 @@
 		}
 		loadMap(mapData);
 
+		var isPointColliding = function (pos, map) {
+			var x = Math.floor(pos.x / tileSize);
+			var y = Math.floor(pos.y / tileSize);
+			if (map[y][x] === 1) return true;
+			return false;
+		}
+
+		var isColliding = function (man, map) {
+			//find out which cell each corner is in.
+			//If a corner is inside a solid square, return true.
+			var corner = man.pos.clone();
+			if (isPointColliding(corner, map)) return true;
+			if (isPointColliding(corner.moveXY(man.size.x-1,0), map)) return true;
+			if (isPointColliding(corner.moveXY(0,man.size.y-1), map)) return true;
+			if (isPointColliding(corner.moveXY(-man.size.x+1,0), map)) return true;
+			return false;
+		}
+
+		var man = {};
+		man.pos = new Pos(50,10);
+		man.size = new Pos(5,5);
+		man.state = "falling";
 
 		man.isOnGround = function () {
-			return this.pos.y >= 90;
+			var leftFoot = isPointColliding(this.pos.clone().moveXY(0,this.size.y), map);
+			var rightFoot = isPointColliding(this.pos.clone().moveXY(this.size.x-1,this.size.y), map);
+			return (leftFoot || rightFoot);
+		}
+
+		man.tryMove = function (x, y) {
+			while (x != 0) {
+				var sign = x > 0 ? 1 : -1;
+				this.pos.x += sign;
+				x -= sign;
+				if (isColliding(this, map)) {
+					this.pos.x -= sign;
+					x = 0; //no more movement.
+				}
+			}
+			while (y != 0) {
+				var sign = y > 0 ? 1 : -1;
+				this.pos.y += sign;
+				y -= sign;
+				if (isColliding(this, map)) {
+					this.pos.y -= sign;
+					y = 0; //no more movement.
+				}
+			}
 		}
 
 		var update = function(keyboard) {
@@ -56,9 +98,9 @@
 			var upHit = keyboard.isKeyHit(KeyEvent.DOM_VK_X);
 
 			if (left && !right) {
-				man.pos.x--;
+				man.tryMove(-1,0);
 			} else if (right && !left) {
-				man.pos.x++;
+				man.tryMove(1,0);
 			}
 
 			if (upHit && man.isOnGround()) {
@@ -69,9 +111,9 @@
 
 			if (man.state === "jumping") {
 				if (man.jumpPhase === 1) {
-					man.pos.y -= 2;
+					man.tryMove(0,-2);
 				} else if (man.jumpPhase === 2) {
-					man.pos.y -= 1;
+					man.tryMove(0,-1);
 				}
 
 				man.jumpTime++;
@@ -88,7 +130,7 @@
 				}
 
 			} else if (!man.isOnGround()) {
-				man.pos.y += 1;
+				man.tryMove(0,1);
 			}
 		}
 
@@ -106,7 +148,7 @@
 			map.forEach(function (row, y) {
 				row.forEach(function (value, x) {
 					if (value === 1) {
-						painter.drawSquare(x*10,y*10, "#FFFF00");
+						painter.drawSquare(x*tileSize,y*tileSize, "#FFFF00");
 					}
 				});
 			});
