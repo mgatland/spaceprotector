604d41157c96098a16ec8a972175a6ab43dd53ba
mgatland <mgatland@gmail.com>
2014-07-05 23:27:13 +1200

    Add a fake loading bar for some reason?

--- js/titlestate.js
+++ js/titlestate.js
@@ -1,17 +1,23 @@
 "use strict";
 define(["colors"], function (Colors) {
 	var TitleState = function () {
+		var fakeLoadingProgress = 0;
+		var maxLoadingProgress = 20;
 		this.update = function (keys, painter, Network, Events) {
 			if (keys.start || keys.up || keys.left || keys.right || keys.down ||
 				keys.shoot || keys.jumpIsHeld) {
 				this.transition = true;
-			} else {
+			}
+			if (fakeLoadingProgress < maxLoadingProgress) {
+				fakeLoadingProgress+= 2;
 			}
 		};
 		this.draw = function (painter) {
-			painter.drawText(40, 20, "Space Protector", Colors.good);
-			painter.drawText(40, 50, "> Press Start  ", Colors.good);
-			painter.drawText(10, 84, "(c)1986 new north road", Colors.good);
+			if (fakeLoadingProgress >= maxLoadingProgress) {
+				painter.drawText(40, 20, "Space Protector", Colors.good);
+				painter.drawText(40, 50, "> Press Start  ", Colors.good);
+			}
+			painter.drawText(20, 74, Array(fakeLoadingProgress+1).join("…"), Colors.good);
 			painter.drawAbsRect(0, 0, 192, 104, Colors.bad, 10);
 		};
 	}
